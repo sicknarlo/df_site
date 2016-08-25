df8c52f819b7542ae1be71771a3c3847eb4e4910
sicknarlo <nicholas.l.sarlo@gmail.com>
2016-08-25 15:38:51 -0500

    update to august

--- imports/ui/ADPConst.jsx
+++ imports/ui/ADPConst.jsx
@@ -1,28 +1,28 @@
 const PValues = {
     ppr: {
       past6MonthsValue: [
-        'feb_16_value',
         'mar_16_value',
         'apr_16_value',
         'may_16_value',
         'jun_16_value',
         'jul_16_value',
+        'aug_16_value',
       ],
       past6MonthsADP: [
-        'feb_16',
         'mar_16',
         'apr_16',
         'may_16',
         'jun_16',
         'jul_16',
+        'aug_16',
       ],
       chartLabels: [
-        'Feb',
         'Mar',
         'Apr',
         'May',
         'Jun',
         'Jul',
+        'Aug',
       ],
       redraftRank: 'redraft_rank',
       rank: 'rank',
@@ -32,28 +32,28 @@ const PValues = {
   },
   super: {
     past6MonthsValue: [
-      'feb_16_value_2qb',
       'mar_16_value_2qb',
       'apr_16_value_2qb',
       'may_16_value_2qb',
       'jun_16_value_2qb',
       'jul_16_value_2qb',
+      'aug_16_value_2qb',
     ],
     past6MonthsADP: [
-      'feb_16_2qb',
       'mar_16_2qb',
       'apr_16_2qb',
       'may_16_2qb',
       'jun_16_2qb',
       'jul_16_2qb',
+      'aug_16_2qb',
     ],
     chartLabels: [
-      'Feb',
       'Mar',
       'Apr',
       'May',
       'Jun',
       'Jul',
+      'Aug',
     ],
     redraftRank: 'redraft_rank_2qb',
     rank: 'rank_2qb',
@@ -61,6 +61,6 @@ const PValues = {
     winNowIndex: 'win_now_index_2qb',
     trend: 'trend_2qb',
   },
-}
+};
 
 export default PValues;
