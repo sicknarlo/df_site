89b6019db5694aabd6e03af70d682aed9187182b
sicknarlo <nicholas.l.sarlo@gmail.com>
2017-05-05 16:09:06 -0500

    fix faw

--- imports/ui/FAQ.jsx
+++ imports/ui/FAQ.jsx
@@ -2,55 +2,9 @@ import React, { Component, PropTypes } from 'react';
 import ReactHighcharts from 'react-highcharts';
 import PValues from './ADPConst.jsx';
 
-// ADPGraph component - represents a ADPGraph profile
-export default class ADPGraph extends Component {
+export default class FAQ extends Component {
 
   render() {
-    const datasets = this.props.players.map((player) => {
-      const data = [];
-      for (var i=0; i<this.props.values.chartLabels.length; i++) {
-        data.push(
-          player[this.props.values.past6MonthsADP[i]]
-        )
-      }
-      return {
-        name: player.name,
-        data,
-      }
-    });
-    const config = {
-      chart: {
-        backgroundColor: "rgba(0,0,0,0)",
-        type: 'line',
-        style: {
-          fontFamily: 'open sans',
-        },
-      },
-      title: {
-        text: 'Average Draft Position',
-      },
-      colors: ['rgba(26,179,148,0.5)', '#1c84c6', '#23c6c8', '#f8ac59', '#ed5565'],
-      xAxis: {
-        categories: this.props.values.chartLabels,
-      },
-      series: datasets,
-      yAxis: {
-        reversed: true,
-        title: {
-          text: undefined,
-        }
-      },
-      plotOptions: {
-        series: {
-          compare: 'percent'
-        }
-      },
-
-      tooltip: {
-        pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y}</b><br/>',
-        valueDecimals: 1,
-      },
-    };
     return (
       <div className="wrapper wrapper-content animated fadeInRight">
         <div className="row">
--- imports/ui/Team.jsx
+++ imports/ui/Team.jsx
@@ -12,33 +12,41 @@ export default class Team extends Component {
         <div className="row  border-bottom white-bg dashboard-header">
           <div className="col-sm-12 text-center">
             <h1>You are not logged in.</h1>
-            <h3><Link to="/tools/login">Log In</Link> or <Link to="/tools/signup">Sign Up</Link> to use the dashboard. It's free!</h3>
+            <h3>
+              <Link to="/tools/login">Log In</Link>
+              {' '}
+              or
+              {' '}
+              <Link to="/tools/signup">Sign Up</Link>
+              {' '}
+              to use the dashboard. It's free!
+            </h3>
           </div>
         </div>
       </div>
-    )
+    );
   }
 
   renderLoggedIn() {
-    const team = this.props.teams.find((t) => t._id === this.props.params.teamID);
+    const team = this.props.teams.find(t => t._id === this.props.params.teamID);
     if (team) {
-      return <TeamPage
-                  team={team}
-                  players={this.props.players}
-                  currentUser={this.props.currentUser}
-                  newsAlerts={this.props.newsAlerts}
-                  values={this.props.values}
-                  currentDb={this.props.currentDb} />
+      return (
+        <TeamPage
+          team={team}
+          players={this.props.players}
+          currentUser={this.props.currentUser}
+          newsAlerts={this.props.newsAlerts}
+          values={this.props.values}
+          currentDb={this.props.currentDb}
+        />
+      );
     } else {
       browserHistory.push('/tools/dashboard');
     }
   }
 
   render() {
-
-    return this.props.currentUser
-      ? this.renderLoggedIn()
-      : this.renderLoggedOut();
+    return this.props.currentUser ? this.renderLoggedIn() : this.renderLoggedOut();
   }
 }
 
