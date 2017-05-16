a6440262b7ca241cb7a30ceaa3d51ab3957ec7db
sicknarlo <nicholas.l.sarlo@gmail.com>
2017-05-16 01:53:23 -0500

    optimize player load and add weekly fp updates and weekly player mfl updates

--- imports/api/cache.js
+++ imports/api/cache.js
@@ -1,7 +1,3 @@
-// import NodeCache from 'node-cache';
-// export const cache = new NodeCache();
-
 import cache from 'memory-cache';
-// export const cache ;
 
 const appcache = module.exports = { cache };
--- server/main.js
+++ server/main.js
@@ -6,37 +6,154 @@ import '../imports/api/drafts.js';
 import '../imports/api/trades.js';
 import '../imports/api/adp.js';
 import '../imports/api/cache.js';
+import { Players } from '../imports/api/players.js';
 import { Meteor } from 'meteor/meteor';
 import cron from 'cron';
 import rp from 'request-promise';
 import cheerio from 'cheerio';
 
-Meteor.startup(() => {
-  var options = {
+function cleanName(name) {
+  return name.replace(' Jr.', '').replace(/\./g, '');
+}
+
+const positions = ['QB', 'WR', 'RB', 'TE'];
+
+function updatePlayers() {
+  const options = {
+    uri: 'http://www03.myfantasyleague.com/2017/export?TYPE=players&DETAILS=1&JSON=1',
+  };
+
+  let n = 0;
+  rp(options).then(
+    Meteor.bindEnvironment(x => {
+      const players = JSON.parse(x).players.player.filter(y => positions.indexOf(y.position) > -1);
+      Meteor.call('players.getPlayers', (err, result) => {
+        result.forEach(p => {
+          if (p.position !== 'PICK') {
+            const newPlayer = p;
+            const match = players.find(z => parseInt(z.id) === parseInt(newPlayer.id));
+            if (match) {
+              n++;
+              newPlayer.birthdate = new Date(parseInt(match.birthdate) * 1000);
+              newPlayer.draft_year = match.draft_year;
+              newPlayer.nfl_id = match.nfl_id;
+              newPlayer.rotoworld_id = match.rotoworld_id;
+              newPlayer.stats_id = match.stats_id;
+              newPlayer.position = match.position;
+              newPlayer.stats_global_id = match.stats_global_id;
+              newPlayer.espn_id = match.espn_id;
+              newPlayer.kffl_id = match.kffl_id;
+              newPlayer.weight = match.weight;
+              newPlayer.draft_team = match.draft_team;
+              newPlayer.draft_pick = match.draft_pick;
+              newPlayer.college = match.college;
+              newPlayer.height = match.height;
+              newPlayer.jersey = match.jersey;
+              newPlayer.twitter_username = match.twitter_username;
+              newPlayer.sportsdata_id = match.sportsdata_id;
+              newPlayer.team = match.team;
+              newPlayer.cbs_id = match.cbs_id;
+            } else {
+              newPlayer.inactive = true;
+            }
+            Players.update({ id: newPlayer.id }, newPlayer);
+          }
+        });
+      });
+    })
+  );
+}
+
+updatePlayers();
+
+function getFantasyProsRankings() {
+  const options = {
     uri: 'https://www.fantasypros.com/nfl/rankings/dynasty-overall.php',
-    transform: function (body) {
-        return cheerio.load(body);
-    }
+    transform(body) {
+      return cheerio.load(body);
+    },
   };
 
   rp(options)
-      .then(function ($) {
-          // Process html like you would with jQuery...
-          const raw = $('[class^="player-label"]');
-          const players = raw.slice(2, raw.length);
-          console.log(players[0].children[0].children[0].data);
-      })
-      .catch(function (err) {
-          // Crawling failed or Cheerio choked...
-      });
-  const job1 = new cron.CronJob({
-    cronTime: '* * * * *',
-    onTick: function() {
-      console.log('job 1 ticked');
-    },
-    start: false,
-    timeZone: 'America/Los_Angeles'
-  });
+    .then(
+      Meteor.bindEnvironment(function ($) {
+        // Process html like you would with jQuery...
+        const raw = $('[class^="mpb-player"]');
+        const players = raw
+          .map((i, x) => {
+            const name = cleanName(x.children[2].children[0].children[0].data);
+            const rank = x.children[0].children[0].data;
+            const pos = x.children[4].children[0].data;
+            const isQb = x.children[4].children[0].data.indexOf('QB') > -1;
+            const superScore = isQb ? rank - 35 : rank;
+            return {
+              name,
+              rank,
+              pos,
+              superScore,
+            };
+          })
+          .toArray();
 
-  job1.start();
+        const playersSuper = players.slice().sort((a, b) => a.superScore - b.superScore);
+        const playersFinal = playersSuper.map((x, i) => {
+          const obj = x;
+          obj.super = i + 1;
+          return obj;
+        });
+        Meteor.call('players.getPlayers', (err, result) => {
+          result.forEach(p => {
+            if (p.position !== 'PICK') {
+              const newPlayer = p;
+              const match = playersFinal.find(x => x.name === newPlayer.name);
+              const newRank = {};
+              newRank.time = new Date();
+              newRank.rank = match ? match.rank : null;
+              newRank.rank_2qb = match ? match.super : null;
+              newRank.redraft = newPlayer.rankings[0] ? newPlayer.rankings[0].redraft : null;
+              newRank.redraft_2qb = newPlayer.rankings[0]
+                ? newPlayer.rankings[0].redraft_2qb
+                : null;
+              newRank.buyindex = match && newPlayer.adp[0].adp
+                ? newPlayer.adp[0].adp - match.rank
+                : 0;
+              newRank.buyindex_2qb = match && newPlayer.adp[0].adp_2qb
+                ? newPlayer.adp[0].adp_2qb - match.super
+                : 0;
+              newRank.rookie = newPlayer.rankings[0].rookie;
+              newRank.rookie_2qb = newPlayer.rankings[0].rookie_2qb;
+              if (!newPlayer.rankings) newPlayer.rankings = [];
+              newPlayer.rankings.unshift(newRank);
+              Players.update({ id: newPlayer.id }, newPlayer);
+            }
+          });
+        });
+      })
+    )
+    .catch(
+      Meteor.bindEnvironment(function (err) {
+        // Crawling failed or Cheerio choked...
+        console.log(err, 'err');
+      })
+    );
+}
+
+const job1 = new cron.CronJob({
+  // cronTime: '00 30 2 * * *',
+  cronTime: '00 30 2 * * 3',
+  onTick: Meteor.bindEnvironment(function () {
+    getFantasyProsRankings();
+  }),
+  start: true,
+  timeZone: 'America/Los_Angeles',
+});
+
+const job2 = new cron.CronJob({
+  // cronTime: '00 30 2 * * *',
+  cronTime: '00 30 2 * * 2',
+  onTick: Meteor.bindEnvironment(function () {
+    updatePlayers();
+  }),
+  start: true,
+  timeZone: 'America/Los_Angeles',
 });
